17d934d0790f1d5ce1dc2061ca42dcc95d6194e5
Luis Antonio Dominguez <ladominguez@ucla.edu>
2024-04-18 06:32:12 -0600

    adding search grid

--- Results/plot_map.py
+++ Results/plot_map.py
@@ -21,7 +21,10 @@ sf = shp.Reader("./zip/1ra_Red_vial_primaria_acceso_controlado_cdmx_09_23.shp")
 proj_string = "+proj=utm +zone={} +ellps=WGS84".format(zone)
 proj_utm = Proj(proj_string)
 fault = np.loadtxt('../coordinates/fault.dat')
+marker_size = 8
 
+def mouse_event(event):
+    print('x: {} and y: {}'.format(event.xdata, event.ydata))
 
 def parse_parameters(input_file):
     parameters = input_file.split('_')
@@ -34,6 +37,7 @@ def parse_parameters(input_file):
 def plot_map(catalog, config):
     #fig = pygmt.Figure()
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 10))
+    cid = fig.canvas.mpl_connect('button_press_event', mouse_event)
     #pygmt.config(MAP_FRAME_TYPE="plain")
     #pygmt.config(FORMAT_GEO_MAP="ddd.xxx")
     #fig.basemap(region=region, projection='M5i', frame=['WSen', 'xa0.015','ya0.015'])
@@ -57,22 +61,58 @@ def plot_map(catalog, config):
     lat = []
 
     for _, row in catalog.iterrows():
+        if row['CC'] < 0.95:
+            ax.plot(float(row['longitude']), float(row['latitude']), 'ko', markersize=6, alpha=0.5, markerfacecolor='gray')
+
+    for _, row in catalog.iterrows():
         if row['CC'] >= 0.95:
             lon.append(row['longitude'])
             lat.append(row['latitude'])
-            ax.plot(float(row['longitude']), float(row['latitude']), 'ko', markersize=10, alpha=0.5, markerfacecolor='red')
+            ax.plot(float(row['longitude']), float(row['latitude']), 'ko', markersize=10, alpha=0.85, markerfacecolor='red', lw=2)
             dlat = config['R']
+            dinc = config['I']
 
             x_square = [row['longitude'] - dlat, row['longitude'] - dlat, row['longitude'] + dlat, row['longitude'] + dlat, row['longitude'] - dlat]
             y_square = [row['latitude'] - dlat, row['latitude'] + dlat, row['latitude'] + dlat, row['latitude'] - dlat, row['latitude'] - dlat]
-            ax.plot(x_square, y_square, 'k-', linewidth=1)
-    ax.plot(fault[:,0], fault[:,1], 'k-', linewidth=2)
+            x_inc = [row['longitude'] - dinc, row['longitude'] - dinc, row['longitude'] + dinc, row['longitude'] + dinc, row['longitude'] - dinc]
+            y_inc = [row['latitude'] - dinc, row['latitude'] + dinc, row['latitude'] + dinc, row['latitude'] - dinc, row['latitude'] - dinc]
+            x_inc2 = [row['longitude'] - dlat, row['longitude'] - dlat, row['longitude'] + dlat, row['longitude'] + dlat, row['longitude'] - dlat]
+            y_inc2 = [row['latitude'] - dinc, row['latitude'] + dinc, row['latitude'] + dinc, row['latitude'] - dinc, row['latitude'] - dinc]
+            x_inc3 = [row['longitude'] - dinc, row['longitude'] - dinc, row['longitude'] + dinc, row['longitude'] + dinc, row['longitude'] - dinc]
+            y_inc3 = [row['latitude'] - dlat, row['latitude'] + dlat, row['latitude'] + dlat, row['latitude'] - dlat, row['latitude'] - dlat]
+            x_inc4 = [row['longitude']      , row['longitude']      , row['longitude']      , row['longitude']      , row['longitude']      ]
+            y_inc4 = [row['latitude'] - dinc, row['latitude'] + dinc, row['latitude'] + dinc, row['latitude'] - dinc, row['latitude'] - dinc]
+            x_inc5 = [row['longitude'] - dinc, row['longitude'] - dinc, row['longitude'] + dinc, row['longitude'] + dinc, row['longitude'] - dinc]
+            y_inc5 = [row['latitude']        , row['latitude']        , row['latitude']        , row['latitude']        , row['latitude']        ]
+            x_inc5 = [row['longitude']       , row['longitude']       , row['longitude'] + dlat, row['longitude'] - dlat, row['longitude']]
+            y_inc5 = [row['latitude']  +dlat , row['latitude']  - dlat, row['latitude']        , row['latitude']        , row['latitude'] ]
+            x_inc6 = [row['longitude']       , row['longitude']       , row['longitude'] + dinc, row['longitude'] - dinc, row['longitude']]
+            y_inc6 = [row['latitude']  +dinc , row['latitude']  - dinc, row['latitude']        , row['latitude']        , row['latitude'] ]
+            ax.plot(x_square, y_square, 'k-', linewidth=1.5)
+            ax.plot(x_square, y_square, 'r+', linewidth=1, markersize=marker_size)
+            ax.plot(x_inc, y_inc, 'r+', linewidth=1, markersize=marker_size)
+            ax.plot(x_inc2, y_inc2, 'r+', linewidth=1, markersize=marker_size)
+            ax.plot(x_inc3, y_inc3, 'r+', linewidth=1, markersize=marker_size)
+            ax.plot(x_inc4, y_inc4, 'r+', linewidth=1, markersize=marker_size)
+            ax.plot(x_inc5, y_inc5, 'r+', linewidth=1, markersize=marker_size)
+            ax.plot(x_inc6, y_inc6, 'r+', linewidth=1, markersize=marker_size)
+        
+    #ax.plot(fault[:,0], fault[:,1], 'r+', linewidth=1, markersize=12)
+    #ax.plot(-99.190, 19.369, 'k*', markersize=18, alpha=0.5, markerfacecolor='yellow')
+    #ax.plot(-99.199, 19.363, 'k*', markersize=18, alpha=0.5, markerfacecolor='green')
     ax.set_xlabel('Longitude')
     ax.set_ylabel('Latitude')
     ax.set_xlim(-99.22, -99.15)
     ax.set_ylim(19.340, 19.410)
     ax.set_aspect('equal', adjustable='box')
     ax.title.set_text('DL: ' + str(np.round(dlat*111.1*1000)) +  'm I: ' + str(np.round(config['I']*111.1*1000)) + 'm MAD: ' + str(config['mad']) )
+    ax.text(-99.18809090909091,19.381363636363634,'Insurgentes', fontsize=12, rotation=85, ma='left', va='bottom')
+    ax.text(-99.16904545454545, 19.35781818181818,'Circuito Interior', fontsize=12, rotation=-5, ma='left', va='bottom')
+    ax.text(-99.20659090909092, 19.368181818181817,'Alta Tension', fontsize=12, rotation=75, ma='left', va='bottom')
+    ax.text(-99.20440909090909, 19.341590909090908,'Periferico', fontsize=12, rotation=80, ma='left', va='bottom')
+    #fig.show() 
+    #fig.show() 
+    fig.savefig('map_dl_' + str(np.round(dlat*111.1*1000)) + 'm.png')
     #fig.show() 
     fig.savefig('map_dl_' + str(np.round(dlat*111.1*1000)) + 'm.png')
 
