f8a07f8bafd3b55fd36f3e6077f948d614691155
Luis Antonio Dominguez <ladominguez@ucla.edu>
2024-04-23 22:59:53 -0600

    December results added

--- plot_catalog.py
+++ plot_catalog.py
@@ -19,10 +19,19 @@ config = 'mad_9_G_1_0.01_1_R_0.003_0.003_0.1_I_0.0015_0.0015_0.05_T_2.0_0_2'
 config = 'mad_9_G_1_0.01_1_R_0.004_0.004_0.1_I_0.002_0.002_0.05_T_3.0_0_3'
 config = 'mad_9_G_1_0.01_1_R_0_0_0_I_0.0015_0.0015_0.05_T_2.0_0_2'
 config = 'mad_9_G_1_0.01_1_R_0.004_0.004_0.1_I_0.002_0.002_0.05_T_2.0_0_2'
-input_file = os.path.join(config,'DetectedFinal_All_20230501_20230531_' + config + '.dat') 
-repeaters_file = os.path.join('possible_repeaters.dat') 
 
-repeater = [19.3620,  -99.2060]
+# December
+#config = 'mad_9_G_1_0.01_1_R_0.002_0.002_0.1_I_0.001_0.001_0.05_T_2.0_0_2'
+#config = 'mad_9_G_1_0.01_1_R_0.003_0.003_0.1_I_0.0015_0.0015_0.05_T_2.0_0_2'
+config = 'mad_9_G_1_0.01_1_R_0.004_0.004_0.1_I_0.002_0.002_0.05_T_2.0_0_2'
+
+input_file = os.path.join('December_20231201_20231231_' + config,'Detected_December_20231201_20231231_' + config + '.dat') 
+
+repeaters_flag = False
+
+if repeaters_flag:
+    repeaters_file = os.path.join('possible_repeaters.dat') 
+    repeater = [19.3620,  -99.2060]
 
 
 top3 = os.path.join(config,'top3.dat')
@@ -38,20 +47,21 @@ if __name__ ==  '__main__':
     catalog =  pd.read_csv(input_file, delim_whitespace=True, header = 1, names=names, dtype=dtypes)
     catalog = catalog[catalog['MAD'] >= mad]
 
-    catalog_temp = pd.read_csv('catalog.dat', delim_whitespace=True, names = ['date', 'latitude', 'longitude', 'Depth', 'Mag', 'latitude2', 'longitude2', 'depth2'],
+    catalog_temp = pd.read_csv('catalog_december.dat', delim_whitespace=True, names = ['date', 'latitude', 'longitude', 'Depth', 'Mag', 'latitude2', 'longitude2', 'depth2'],
                                dtype={'date': str, 'latitude': float, 'longitude': float, 'Depth': float, 'Mag': float, 'latitude2': float, 'longitude2': float, 'depth2': float})
     catalog_temp['Date'] = pd.to_datetime(catalog_temp['date'], format='%Y%m%d%H%M%S.%f')
 
-    repeaters = pd.read_csv(repeaters_file, delim_whitespace=True, names=names, dtype=dtypes)
-    top3 = pd.read_csv(top3, delim_whitespace=True, names=['N','Reference'], dtype = {'N': int,'Reference': str})
+    #top3 = pd.read_csv(top3, delim_whitespace=True, names=['N','Reference'], dtype = {'N': int,'Reference': str})
 
     catalog['Date'] = catalog['Date'].str.cat(catalog['Time'], sep=' ')
     catalog['Date'] = pd.to_datetime(catalog['Date'], format='%Y/%m/%d %H:%M:%S.%f')
     catalog = catalog.drop(columns=['Time'])
 
-    repeaters['Date'] = repeaters['Date'].str.cat(repeaters['Time'], sep=' ')
-    repeaters['Date'] = pd.to_datetime(repeaters['Date'], format='%Y/%m/%d %H:%M:%S.%f')
-    repeaters = repeaters.drop(columns=['Time'])
+    if repeaters_flag:
+        repeaters = pd.read_csv(repeaters_file, delim_whitespace=True, names=names, dtype=dtypes)
+        repeaters['Date'] = repeaters['Date'].str.cat(repeaters['Time'], sep=' ')
+        repeaters['Date'] = pd.to_datetime(repeaters['Date'], format='%Y/%m/%d %H:%M:%S.%f')
+        repeaters = repeaters.drop(columns=['Time'])
 
     norm = plt.Normalize(catalog['Depth'].min(), catalog['Depth'].max())
     depths = catalog['Depth']
@@ -89,18 +99,19 @@ if __name__ ==  '__main__':
                             popup=row['Date'].strftime('%Y/%m/%d %H:M:S') + ' ' + str(row['Mag']),
                             fill_color=color, # divvy color
                            ).add_to(m)
-    for index, row in top3.iterrows():
-        subcat = catalog.loc[catalog['Reference']==row['Reference']]
-        subcat.reset_index(drop=True, inplace=True)
-        ind_max = subcat['MAD'].idxmax()
-        folium.CircleMarker([subcat.iloc[ind_max]['latitude'], subcat.iloc[ind_max]['longitude']], popup=row['N'], fill_color='blue', radius = 20).add_to(m)
+    #for index, row in top3.iterrows():
+    #    subcat = catalog.loc[catalog['Reference']==row['Reference']]
+    #    subcat.reset_index(drop=True, inplace=True)
+    #    ind_max = subcat['MAD'].idxmax()
+    #    folium.CircleMarker([subcat.iloc[ind_max]['latitude'], subcat.iloc[ind_max]['longitude']], popup=row['N'], fill_color='blue', radius = 20).add_to(m)
     folium.Marker(location=[19.375,  -99.1707], popup='BJVM').add_to(m)
     folium.Marker(location=[19.3511,  -99.1562], popup='COVM').add_to(m)
     folium.Marker(location=[19.4080, -99.2091], popup='MHVM').add_to(m)
     folium.Marker(location=[19.3669, -99.1931], popup='ENP8').add_to(m)
     folium.Marker(location=[19.329,  -99.178 ], popup='PZIG').add_to(m)
 
-    folium.CircleMarker(repeater, fill_color='red', fill_opacity=1,radius = 10, weigth=5, opacity=1, popup='Repeater').add_to(m)
+    if repeaters_flag:
+        folium.CircleMarker(repeater, fill_color='red', fill_opacity=1,radius = 10, weigth=5, opacity=1, popup='Repeater').add_to(m)
 
     fig, ax = plt.subplots(2, 1, figsize=(16, 4), sharex=True)
     markerlines, stemlines, _ = ax[0].stem(catalog['Date'], catalog['Mag'], 'o', linefmt='-', basefmt='black')
@@ -113,9 +124,10 @@ if __name__ ==  '__main__':
         else:
             ax[0].plot(row['Date'], row['Mag'], 'o', color='blue')
 
-    for index, row in repeaters.iterrows():
-        print(row['Date'], row['Mag'])
-        ax[0].plot(row['Date'], row['Mag'], 'k*', mec='black', mfc='yellow', markersize=12)
+    if repeaters_flag:
+        for index, row in repeaters.iterrows():
+            print(row['Date'], row['Mag'])
+            ax[0].plot(row['Date'], row['Mag'], 'k*', mec='black', mfc='yellow', markersize=12)
 
 
     ax[0].set_xlabel('Date')
@@ -139,8 +151,8 @@ if __name__ ==  '__main__':
     #print('No of template detections:', len(template_detection))
     print(all_detection)
     print('No of all detections:', len(all_detection))
-
-    ax[1].plot(repeaters['Date'], repeaters['Depth'], 'k*', mec='black', mfc='yellow', markersize=16)
+    if repeaters_flag:
+        ax[1].plot(repeaters['Date'], repeaters['Depth'], 'k*', mec='black', mfc='yellow', markersize=16)
     cbar.set_label('Magnitude')
 
     #ax[1].plot(catalog['Date'], catalog['Depth'], 'o', markersize=2)
@@ -153,6 +165,6 @@ if __name__ ==  '__main__':
     #ax[2].set_ylabel('CC')
     #ax[2].grid(True)
     #plt.tight_layout()
-    plt.savefig(f'Figure_{config}.png', dpi=300)
+    plt.savefig(f'Figure_December_{config}.png', dpi=300)
     #m.show_in_browser()
-    m.save(f'map_mad_{config}.html')  
+    m.save(f'map_mad_December_{config}.html')  
